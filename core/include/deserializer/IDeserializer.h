44c907d6d330c0888343e925dd9dc185347e7d21
jimmysecai <jimmy@secai.ca>
2026-05-20 17:02:39 -0500

    Better implementation of rule of 5 for serializer and deserializer interface

--- core/include/deserializer/IDeserializer.h
+++ core/include/deserializer/IDeserializer.h
@@ -6,7 +6,14 @@ template<typename TData, typename TDeserialized>
 class IDeserializer
 {
 public:
+    IDeserializer() = default;
     virtual ~IDeserializer() = default;
+
+    // Rule of Five: Explicitly delete copy/move to prevent slicing
+    IDeserializer(const IDeserializer&) = delete;
+    IDeserializer& operator=(const IDeserializer&) = delete;
+    IDeserializer(IDeserializer&&) = delete;
+    IDeserializer& operator=(IDeserializer&&) = delete;
 
     virtual TDeserialized Deserialize(const TData& Data) = 0;
 };
--- core/include/serializer/ISerializer.h
+++ core/include/serializer/ISerializer.h
@@ -6,7 +6,14 @@ template<typename TData, typename TSerialized>
 class ISerializer
 {
 public:
+    ISerializer() = default;
     virtual ~ISerializer() = default;
+
+    // Rule of Five: Explicitly delete copy/move to prevent slicing
+    ISerializer(const ISerializer&) = delete;
+    ISerializer& operator=(const ISerializer&) = delete;
+    ISerializer(ISerializer&&) = delete;
+    ISerializer& operator=(ISerializer&&) = delete;
 
     virtual TSerialized Serialize(const TData& Data) = 0;
 };
